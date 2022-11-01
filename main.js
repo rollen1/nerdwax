a54d60b42d8d7af464fe34ed7d3448cadb5ce27b
rollen1 <nolovedivision@gmail.com>
2022-11-01 17:36:58 +0200

    js

---
+++ main.js
@@ -0,0 +1,52 @@
+function defaultContent() {
+    document.getElementById("content").innerHTML
+        = `<img src="assets/default-desktop.jpg" alt="default-desktop">
+        <p>This thinner, slant-tipped container provides ease of application once only dreamed of!</p>
+    <p>Nerdwax is a beeswax-based blend of All Natural and Certified Organic Ingredients specially formulated to
+        keep your
+        glasses in place while you live your life.</p>
+    <p>Nerdwax works by creating a layer of friction between your
+        glasses and your skin. So it doesn't matter what type of glasses you have, as long as they fit properly
+        nerdwax will keep them in place! It's also super easy to apply: Simply rub nerdwax on the nose pads of
+        your
+        favorite specs or sunnies and enjoy slip free bliss.</p>
+    <p>We pride ourselves in our commitment to using only the
+        best All-Natural, Cosmetic Grade Ingredients.</p>
+    <p> We use Certified Organic and Sustainably Sourced products
+        whenever possible.</p>
+    <p class="bolded">Ingredients: Beeswax, Coconut Oil, Gum Rosin</p>`
+}
+
+function magicContent() {
+    document.getElementById("content").innerHTML
+        = `<img src="assets/magic-desktop.jpg" alt="magic-desktop">
+        <p>Nerdwax Magic Drops are guaranteed to provide a magical clean for your sunnies, specs, and screens!</p>
+    <p>Just 1-2
+        little drops of the magic formula and wipe clean with the included nerdwax Microfiber cloth.</p>
+    <p>Next thing you
+        know you'll be singing to the heavens… oh oh it's magic</p>
+
+    <p class="bolded">Magic drops net wt. 0.17oz/5ml</p>
+    <p class="bolded"> Magic drops refill net wt. 2oz/59ml</p>`
+}
+
+function clipContent() {
+    document.getElementById("content").innerHTML
+        = `<img src="assets/clip-cloth-desktop.jpg" alt="clip-cloth-desktop">
+        <p>Slap this baby onto one our Secretly Nerdy microfiber cloths, clip it to your bag, and you're good to go!
+        </p>
+        <p>Holds on nice and tight so you don't have to worry about losing your cloth.</p>`
+}
+
+function clothContent() {
+    document.getElementById("content").innerHTML
+        = `<img src="assets/cloth-desktop.jpg" alt="cloth-desktop">
+        <p>There's a lot of things you need to get across this universe. Warp drive… wormhole refractors… </p>
+        <p>You know the
+            thing you need most of all? A blue herringbone cleaning cloth that will leave your spectacles spick and
+            span. </p>
+        <p>All Secretly Nerdy microfiber cloths are made from recycled plastic water bottles and guaranteed to
+            remove Nerdwax® from lenses.</p>
+        <p class="bolded">Dimensions: 12" X 12"</p>`
+}
+
